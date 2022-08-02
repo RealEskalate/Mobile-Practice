7d852a058960cdd06dbbb716a1f68be8a43cafdc
Tesfaye <adugnatesfaye888@gmail.com>
2022-08-02 11:28:45 +0300

    [mobile] added the tags feature to new_articles page

--- start_project_blog_g2/lib/screens/new_article.dart
+++ start_project_blog_g2/lib/screens/new_article.dart
@@ -11,118 +11,113 @@ class NewArticlePage extends StatefulWidget {
 
 class _MyHomePageState extends State<NewArticlePage> {
   final _formKey = GlobalKey<FormState>();
+  final tagController = TextEditingController();
+
+  final listOfTags = [];
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.fromLTRB(40, 40, 40, 0), children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Text(
-              "New Article",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Add title field
-                  TextFormField(
-                    decoration: const InputDecoration(hintText: "Add title"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // header text
+              const Text(
+                "New Article",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              // The form
+              Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Add title field
+                    TextFormField(
+                      decoration: const InputDecoration(hintText: "Add title"),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
+                    // Add subtitle field
+                    TextFormField(
+                      decoration:
+                          const InputDecoration(hintText: "Add subtitle"),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some text';
+                        }
+                        return null;
+                      },
+                    ),
+                    // Add tags field and add icon
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        // add tags field
+                        Container(
+                          width: width - 170,
+                          child: TextFormField(
+                            controller: tagController,
+                            decoration: const InputDecoration(hintText: "Add tags"),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
 
-                  // Add subtitle field
-                  TextFormField(
-                    decoration: const InputDecoration(hintText: "Add subtitle"),
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Please enter some text';
-                      }
-                      return null;
-                    },
-                  ),
-
-                  // Add tags field
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // add tags field
-                      Container(
-                        width: 190,
-                        child: TextFormField(
-                          decoration:
-                              const InputDecoration(hintText: "Add tags"),
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter some text';
+                        // add icon for adding tags.
+                        RawMaterialButton(
+                          onPressed: () {
+                            String newTagValue = tagController.text;
+                            if (newTagValue.isNotEmpty &
+                                !listOfTags.contains(newTagValue) & (newTagValue.length<=11)) {
+                              listOfTags.add(newTagValue);
+                              setState(() {});
                             }
-                            return null;
                           },
+                          elevation: 2.0,
+                          fillColor: Colors.white,
+                          hoverColor: Colors.black12,
+                          child: Icon(Icons.add,
+                              size: 25.0, color: Color(0xff376AED)),
+                          shape: const CircleBorder(),
                         ),
+                      ],
+                    ),
+                    Text(
+                      "add as many tags as you want.",
+                    ),
+                    // Display the tags
+                    SizedBox(
+                      height: height * 0.15, 
+                      child: buildTags()
                       ),
-
-                      // add icon for adding tags. the onPressed function is to be done soon.
-                      RawMaterialButton(
-                        onPressed: () {},
-                        elevation: 2.0,
-                        fillColor: Colors.white,
-                        child: Icon(
-                          Icons.add,
-                          size: 25.0,
-                        ),
-                        padding: EdgeInsets.all(0),
-                        shape: const CircleBorder(),
-                      ),
-                    ],
-                  ),
-                  Text(
-                    "add as many tags as you want.",
-                  ),
-
-                  Chip(
-                      side: BorderSide(color: Colors.blue, width: 2),
-                      backgroundColor: Colors.white,
-                      label: const Text('Aaron Burr'),
-                      deleteIcon: Icon(
-                        Icons.close,
-                      ),
-                      onDeleted: () {
-                        setState(() {});
-                      }),
-
-                  Container(),
-
-                  // Article content
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: TextFormField(
-                      maxLines: 15,
+                    // Article content
+                    TextFormField(
+                      maxLines: 12,
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: 'Article content',
                       ),
                     ),
-                  ),
-
-                  SizedBox(
-                    height: 10,
-                  ),
-
-                  // Cut, image, video, pdf, links, Texts Icons
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: Center(
+                    // Close, image, video, pdf, links, Texts Icons
+                    Center(
                       child: Container(
-                        width: 220,
+                        margin: EdgeInsets.only(top: 20),
+                        width: width * 0.8 - 80,
                         height: 50,
                         decoration: BoxDecoration(
                             color: Color(0xff376AED),
@@ -132,7 +127,7 @@ class _MyHomePageState extends State<NewArticlePage> {
                                 color: Colors.grey.withOpacity(0.5),
                                 spreadRadius: 5,
                                 blurRadius: 7,
-                                offset: Offset(0, 3), // changes position of shadow
+                                offset: Offset(0, 3),
                               ),
                             ]
                           ),
@@ -142,90 +137,106 @@ class _MyHomePageState extends State<NewArticlePage> {
                             SizedBox(
                               width: 0.01,
                             ),
+                            // close icon
                             Container(
                               decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.all(Radius.circular(200))),
-                              child: Icon(
-                                Icons.close,
-                                size: 40,
-                                color: Colors.black,
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.all(Radius.circular(200))),
+                              child: Icon(Icons.close,size: 40, color: Colors.black,),
                               ),
-                            ),
+                            // add photo icon
                             Container(
-                              child: Icon(
-                                Icons.broken_image_outlined,
-                                size: 25,
-                                color: Colors.white,
+                              child: Icon(Icons.broken_image_outlined, size: 25, color: Colors.white,),
                               ),
-                            ),
+                            // add video icon
                             Container(
-                              child: Icon(
-                                Icons.play_circle_outline_outlined,
-                                size: 25,
-                                color: Colors.white,
+                              child: Icon(Icons.play_circle_outline_outlined, size: 25,color: Colors.white,),
                               ),
-                            ),
+                            // add texts icon
                             Container(
-                              child: Icon(
-                                Icons.format_align_left_sharp,
-                                size: 25,
-                                color: Colors.white,
+                              child: Icon(Icons.format_align_left_sharp, size: 25, color: Colors.white,),
                               ),
-                            ),
+                            // add links icon
                             Container(
-                              child: Icon(
-                                Icons.link_sharp,
-                                size: 25,
-                                color: Colors.white,
+                              child: Icon(Icons.link_sharp, size: 25, color: Colors.white,),
                               ),
-                            ),
+                            // add texts icon
                             Container(
-                              child: Icon(
-                                Icons.text_fields_outlined,
-                                size: 25,
-                                color: Colors.white,
+                              child: Icon(Icons.text_fields_outlined, size: 25, color: Colors.white,),
                               ),
-                            ),
-
                             SizedBox(
                               width: 10,
-                            )
-                          ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ),
 
-                  // publish button
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 10.0),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Color(0xff376AED)),
-                            shape: MaterialStateProperty.all<
-                                RoundedRectangleBorder>(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0),
-                        )),
+                    // publish button
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10.0),
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(Color(0xff376AED)),
+                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            )),
+                          ),
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Processing Data')),
+                              );
+                            }
+                          },
+                          child: const Text('Publish'),
                         ),
-                        onPressed: () {
-                          if (_formKey.currentState!.validate()) {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Processing Data')),
-                            );
-                          }
-                        },
-                        child: const Text('Publish'),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ]),
+      ),
+    );
+  }
+
+  Widget buildTags() {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 0,
+        mainAxisSpacing: 0,
+        childAspectRatio: 3.5,
+      ),
+      scrollDirection: Axis.vertical,
+      itemCount: listOfTags.length,
+      itemBuilder: (context, index) {
+        return Row(
+          children: [
+            Chip(
+                side: BorderSide(color: Color(0xff376AED), width: 2),
+                backgroundColor: Colors.white,
+                label: Text(listOfTags[index]),
+                deleteIcon: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.all(Radius.circular(200))),
+                  child: Icon(
+                    Icons.close,
+                  ),
+                ),
+                onDeleted: () {
+                  listOfTags.remove(listOfTags[index]);
+                  setState(() {});
+                }),
+          ],
+        );
+      },
     );
   }
 }
