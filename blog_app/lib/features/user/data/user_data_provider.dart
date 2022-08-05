db72aa2402624500bff95bb90f5512c21992822c
abelkm99 <abelkidanemariam99@gmail.com>
2022-08-05 08:04:30 +0300

    [Mobile] update folder structure and user provider

---
+++ blog_app/lib/features/user/data/user_data_provider.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+import 'package:blog_app/core/http_client.dart';
+import 'package:blog_app/features/user/domain/entities/user.dart';
+import 'package:dio/dio.dart';
+
+class UserProvide {
+  final dio = createDio();
+
+  void getUserModel() async {
+    try {
+      // _clien.dio.get(path)
+      final dio = createDio();
+      var response = await dio.get('/user');
+      print(response.data);
+    } catch (e) {
+      print("object");
+    }
+  }
+
+  Future<User> getCurrentUser() async {
+    String url = "/user";
+
+    var response = await dio.get(url);
+    return User.fromJson(jsonDecode(response.data));
+  }
+
+  Future<List<User>> getAllUser() async {
+    String url = "/user/all";
+    var usersResponse = await dio.get(url);
+    List<dynamic> usersBody = jsonDecode(usersResponse.data);
+    return usersBody.map((userMap) => User.fromJson(userMap)).toList();
+  }
+}
